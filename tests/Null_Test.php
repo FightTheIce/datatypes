e3d52fbec21ac4ff66c5d6edec3af0c39e6f6e88
William Knauss <william.knauss@kingdom.com>
2020-11-12 18:12:57 -0500

    more testing

---
+++ tests/Null_Test.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+use FightTheIce\Datatypes\Special\Null_;
+
+final class Null_Test extends TestCase
+{
+    public function test_is_null() {
+        $null = new Null_;
+        $this->assertNull($null->getValue());
+    }
+}
