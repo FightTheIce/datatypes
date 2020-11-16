eb6d03843a23b753f85c016e40167c0fa0b56292
William Knauss <william.knauss@kingdom.com>
2020-11-16 15:15:21 -0500

    null

--- src/Datatypes/Special/Null_.php
+++ src/Datatypes/Special/Null_.php
@@ -6,10 +6,26 @@ namespace FightTheIce\Datatypes\Special;
 
 use FightTheIce\Datatypes\Core\Contracts\DatatypeInterface;
 use Illuminate\Support\Traits\Macroable;
+use FightTheIce\Exceptions\InvalidArgumentException;
 
 class Null_ implements DatatypeInterface
 {
     use Macroable;
+
+    /**
+     * __construct
+     *
+     * @param   null $val
+     *
+     */
+    public function __construct($val = null) {
+        if (!is_null($val)) {
+            $exception = new InvalidArgumentException('[NULL] is the only valid value.');
+            $exception->setComponentName('datatypes');
+
+            throw $exception;
+        }
+    }
 
     public function getValue()
     {
