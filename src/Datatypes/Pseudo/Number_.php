a815a8f7ed5f90acca44d3c69c2fe2be37830899
William Knauss <william.knauss@kingdom.com>
2020-11-18 22:03:10 -0500

    php cs fix

--- src/Datatypes/Pseudo/Number_.php
+++ src/Datatypes/Pseudo/Number_.php
@@ -7,14 +7,12 @@ namespace FightTheIce\Datatypes\Pseudo;
 use FightTheIce\Datatypes\Core\Contracts\NumberInterface;
 use FightTheIce\Datatypes\Core\Contracts\PseudoNumberInterface;
 use Illuminate\Support\Traits\Macroable;
-
 use FightTheIce\Datatypes\Core\Contracts\BooleanInterface;
 use FightTheIce\Datatypes\Core\Contracts\FloatInterface;
 use FightTheIce\Datatypes\Core\Contracts\IntegerInterface;
 use FightTheIce\Datatypes\Scalar\Boolean_;
 use FightTheIce\Datatypes\Scalar\Integer_;
 use FightTheIce\Datatypes\Scalar\Float_;
-
 use FightTheIce\Exceptions\InvalidArgumentException;
 use Brick\Math\BigNumber;
 
@@ -31,11 +29,12 @@ class Number_ implements NumberInterface, PseudoNumberInterface
     protected NumberInterface $concrete;
 
     /**
-     * __construct
+     * __construct.
      *
-     * @param   mixed $num
+     * @param mixed $num
      */
-    public function __construct($num = 0) {
+    public function __construct($num = 0)
+    {
         if (is_int($num)) {
             $this->concrete = new Integer_($num);
         } elseif (is_float($num)) {
@@ -48,39 +47,48 @@ class Number_ implements NumberInterface, PseudoNumberInterface
         }
     }
 
-    public function is_float(): BooleanInterface {
-        return new Boolean_(($this->concrete instanceOf Float_));
+    public function is_float(): BooleanInterface
+    {
+        return new Boolean_(($this->concrete instanceof Float_));
     }
 
-    public function is_integer(): BooleanInterface {
-        return new Boolean_(($this->concrete instanceOf Integer_));
+    public function is_integer(): BooleanInterface
+    {
+        return new Boolean_(($this->concrete instanceof Integer_));
     }
 
-    public function resolve(): NumberInterface {
+    public function resolve(): NumberInterface
+    {
         return $this->concrete;
     }
 
-    public function getPrimitiveType(): string {
+    public function getPrimitiveType(): string
+    {
         return 'number';
     }
 
-    public function getDatatypeCategory(): string {
+    public function getDatatypeCategory(): string
+    {
         return 'pseudo';
     }
 
-    public function describe(): string {
+    public function describe(): string
+    {
         return $this->concrete->describe();
     }
 
-    public function isPositive(): BooleanInterface {
+    public function isPositive(): BooleanInterface
+    {
         return $this->concrete->isPositive();
     }
 
-    public function isNegative(): BooleanInterface {
+    public function isNegative(): BooleanInterface
+    {
         return $this->concrete->isNegative();
     }
 
-    public function isZero(): BooleanInterface {
+    public function isZero(): BooleanInterface
+    {
         return $this->concrete->isZero();
     }
 
@@ -89,31 +97,38 @@ class Number_ implements NumberInterface, PseudoNumberInterface
      *
      * @return int|float
      */
-    public function getNumber() {
+    public function getNumber()
+    {
         return $this->concrete->getNumber();
     }
 
-    public function __toFloat(): FloatInterface {
+    public function __toFloat(): FloatInterface
+    {
         return $this->concrete->__toFloat();
     }
 
-    public function __toInteger(): IntegerInterface {
+    public function __toInteger(): IntegerInterface
+    {
         return $this->concrete->__toInteger();
     }
 
-    public function absolute(): NumberInterface {
+    public function absolute(): NumberInterface
+    {
         return new self($this->concrete->absolute()->getNumber());
     }
 
-    public function negated(): NumberInterface {
+    public function negated(): NumberInterface
+    {
         return new self($this->concrete->negated()->getNumber());
     }
 
-    public function negativeabsolute(): NumberInterface {
+    public function negativeabsolute(): NumberInterface
+    {
         return new self($this->concrete->negativeabsolute()->getNumber());
     }
 
-    public function math(): BigNumber {
+    public function math(): BigNumber
+    {
         return $this->concrete->math();
     }
 }
